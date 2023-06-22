1ab49e67e136eff0dbc16cb94c9d711d69f72183
Jacob Sniff <jacobsniff18@gmail.com>
2023-06-22 19:27:33 -0400

    added server running code to server.js file NEED define user routes

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const userRoutes = require('./routes/user-routes');
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+app.use('/api/users', userRoutes);
+
+mongoose.connect('mongodb://localhost/socialnetwork', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+});
