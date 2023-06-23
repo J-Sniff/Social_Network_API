32b683f89292b6642c36e9b0c7a4c5367591c369
Jacob Sniff <jacobsniff18@gmail.com>
2023-06-23 15:53:16 -0400

    recoded server.js file and added code to connections. NEED to finish reformating routes

--- server.js
+++ server.js
@@ -1,22 +1,17 @@
 const express = require('express');
-const mongoose = require('mongoose');
-const userRoutes = require('./routes/user-routes');
+const db = require('./config/connection');
+const routes = require('./routes');
 
+const PORT = process.env.PORT || 3001;
 const app = express();
-const PORT = 3000;
 
+
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use('/api/users', userRoutes);
+app.use(routes);
 
-mongoose.set('strictQuery', false);
-
-mongoose.connect('mongodb://localhost/socialnetwork', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    // useCreateIndex: true,
-    // useFindAndModify: false,
-});
-
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
+db.once('open', () => {
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
+  });
 });
